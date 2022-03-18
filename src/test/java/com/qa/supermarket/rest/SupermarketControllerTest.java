24fd6c409c093dde2dd0e27ad4d83f023ee821a5
acockerton <abcockerton@gmail.com>
2022-03-18 11:34:33 +0000

    fixes

--- src/test/java/com/qa/supermarket/rest/SupermarketControllerTest.java
+++ src/test/java/com/qa/supermarket/rest/SupermarketControllerTest.java
@@ -1,6 +1,5 @@
 package com.qa.supermarket.rest;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,7 +20,7 @@ import com.qa.supermarket.domain.Supermarket;
 
 @SpringBootTest
 @AutoConfigureMockMvc 
-@Sql(scripts = {"classpath:supermarket-schema.sql","classpath:supermarket-data.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = {"classpath:supermarket-schema.sql","classpath:supermarket-data.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)// PRE-POPULATE OUT H2 DB
 @ActiveProfiles("test")
 public class SupermarketControllerTest {
 
@@ -33,32 +32,23 @@ private ObjectMapper map;
 
 @Test
 void testCreate() throws Exception {
-
-	Supermarket create = new Supermarket("Peas",500,"Frozen");
+	
+	Supermarket create = new Supermarket("Peas",500,"Frozen");//object
 	
 	String createJSON =this.map.writeValueAsString(create);
 	
 	RequestBuilder mockRequest = post("/create").contentType(MediaType.APPLICATION_JSON).content(createJSON);
 	
-	Supermarket saved = new Supermarket(1L, "Peas",500,"Frozen");
+	Supermarket saved = new Supermarket(2L, "Peas",500,"Frozen");
 	
 	String savedJSON =this.map.writeValueAsString(saved);
 	
 	ResultMatcher matchStatus = status().isCreated();
-
+	
 	ResultMatcher matchBody = content().json(savedJSON);
-
+	
 	this.mock.perform(mockRequest).andExpect(matchStatus).andExpect(matchBody);
 	
-}
-
-@Test
-void testRemove() throws Exception {
-	Long id = 1L;
-	RequestBuilder mockRequest = delete("/remove/?id="+id);
-	ResultMatcher status = status().isOk();
-	ResultMatcher body = content().string("true");
-	this.mock.perform(mockRequest).andExpect(body).andExpect(status);
 }
 
 
